735c9f572b74045358d89e3c59576b34a97e64d4
Simon Stjernholm <simon@stjernholm.dev>
2024-03-29 19:04:14 +0100

    add eslint (#1)
    * add eslint
    
    * fix eslint

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 // Web app framework:
-import express from 'express';
-import http from 'http';
-const app = express();
-const server = http.createServer(app);
+import express from "express"
+import http from "http"
+import { Server } from "socket.io"
+const app = express()
+const server = http.createServer(app)
 
 // Socket server that makes use of the above http app server:
-import { Server } from "socket.io";
 const io = new Server(server, {
   cors: {
     origin: "*"
   }
-});
+})
 
 // Basic app routing
-//app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
 //  res.sendFile(__dirname + '/index.html');
-//});
+// });
 
 // Handle socket connections
-io.on('connection', (socket) => {
-  console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected")
 
-  socket.on('buttonClick', (count) => {
+  socket.on("buttonClick", (count) => {
     console.log(socket.id, "has clicked the button", count, "times")
-  });
-});
+  })
+})
 
 // Start application server
 server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+  console.log("listening on *:3000")
+})
