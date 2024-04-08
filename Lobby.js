5bd4ea20804cd729eb55dbb9610fc964da897d65
glund23student <glund23@student.aau.dk>
2024-04-08 12:15:17 +0200

    added new file lobby.js

---
+++ Lobby.js
@@ -0,0 +1,36 @@
+export {}
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+// ========================================= host lobby ===============================================================
+
+// lobby page loaded
+
+function CreateLobby(socket) {
+    // Create map for rooms
+    
+}
+
+function CreateLobbyID(Map) {
+    
+}
+
+function SendSettings(socket) {
+    
+}
+
+
+//Choose Decks
+// function 
+
+//change Settings 
+
+//Delete Lobby 
+
+// Start Game
+
+
+
+
+
+// ========================================= joined lobby ============================================================== 
--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import express from "express"
 import http from "http"
 import { Server } from "socket.io"
 const app = express()
-const server = http.createServer(app)
+const server = http.createServer(app) 
+
 
 // Socket server that makes use of the above http app server:
 const io = new Server(server, {
@@ -15,7 +16,7 @@ const io = new Server(server, {
 // Basic app routing
 // app.get('/', (req, res) => {
 //  res.sendFile(__dirname + '/index.html');
-// });
+// }); 
 
 // Handle socket connections
 io.on("connection", (socket) => {
@@ -24,6 +25,8 @@ io.on("connection", (socket) => {
   socket.on("buttonClick", (count) => {
     console.log(socket.id, "has clicked the button", count, "times")
   })
+
+  socket.on("lobbyCreate", dsad(socket))
 })
 
 // Start application server
