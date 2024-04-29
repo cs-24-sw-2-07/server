e168960865f7e83ab2d9b244b6de2dc336f45adb
Jacob <144026631+Menacham@users.noreply.github.com>
2024-04-29 09:21:12 +0200

    Lobby events (#12)
    * added new file lobby.js
    
    * started function createdLobby
    
    * Further work on createLobbyID
    
    * Work index --> Trying to import files
    
    * Co-authored-by: glund23student <glund23student@users.noreply.github.com>
    Co-authored-by: aske-design1 <aske-design1@users.noreply.github.com>
    
    * Co-authored-by: aske-design1 <aske-design1@users.noreply.github.com>
    
    * Co-authored-by: aske-design1 <aske-design1@users.noreply.github.com>
    
    * Added Deletelobby function
    
    * Made the lobby object
    
    * Further work on lobby obj
    
    * Added joinlobby function
    
    * Finished working on obj i think
    
    * added read player data map
    
    * Finished changeSetttings function
    
    * Changed joinlobby
    
    * Finished changeSettings function
    
    * work on joinLobby
    
    * test
    
    * Fixing this mess
    
    * you didn't fix anything
    
    * Fixed it
    
    * fixed
    
    * Made function changeDeckState
    
    * Started fixing errors
    
    * finished the functions
    
    * made changes to JoinLobby and LeaveLobby
    
    * better comment for rooms
    
    * Further work on comments
    
    * Completed Join, leave and deleteLobby functions
    
    * Better comments
    
    * comments
    
    * test
    
    * player ready og start game funktioner lavet sammen med dets socket
    
    * bugfix clientserver
    
    * Bugfixing clientsever
    
    * Saved this time
    
    * Bug fixing of lobbyStatObj
    
    * Bugfixing
    
    * Fixed rooms
    
    * Further bugfixing + added comments to events
    
    * Comments
    
    * Further work on backend
    
    * Finished touch on createLobby
    
    * Fixed bug
    
    * Fixed majority of functions
    
    * bugfixing
    
    * Refactored startgame event
    
    * only missing logic now
    
    * added todos
    
    * more todos added
    
    * more todos added
    
    * refactors and more todos
    
    * refactor of test
    
    * cleaned up a little
    
    * Updated SettingsJson
    
    * Further work on server-client integration
    
    * Lobby state backend is now fully functional
    
    * Minor changes to console logs
    
    * minor syntax changes
    
    * Finished up the final work for the backend
    
    * better code now
    
    * little more refactoring
    
    * comments done
    
    * comments done
    
    ---------
    
    Co-authored-by: glund23student <glund23@student.aau.dk>
    Co-authored-by: Aske-design1 <hm64zk@student.aau.dk>

---
+++ Lobby.js
@@ -0,0 +1,239 @@
+//import path from "path";
+import { Rooms } from "./index.js";
+export { CreateLobby, ChangeSettings, JoinLobby, LeaveLobby, DeleteLobby, ChangeDeckState, ShouldStartGame, PlayerReady };
+
+//* =================================================== host lobby =============================================================== *\\
+/**
+ * Creates a lobby for the host that is stored in the Room map
+ * @param {*} socket An instance of the users socket
+ * @param {*} displayName The name that the user entered which will be their username in the lobby 
+ * @returns an object that is sent client-side to the update the lobby accordingly
+ */
+function CreateLobby(socket, displayName) {
+    const id = CreateLobbyID(); 
+    const roomID = `/${id}`;
+    socket.join(roomID);
+
+    // Sets up roomObj and pushes to room map 
+    const settingsJson = { 
+        "deckSize": 15, 
+        "handSize" : 5,
+        "life": 3,
+        "lobbySize": 2
+    };
+    let RoomObj = RoomStateObj(socket, displayName, settingsJson); 
+    Rooms.set(roomID, RoomObj);
+    
+    const playerArr = MapToArrayObj(RoomObj.players);
+    // Sends the default settings and id to the host
+    return {
+        ...settingsJson, 
+        id: id,
+        players: playerArr
+    };
+}
+/**
+ * Creates a lobby-id consisting of 6 numbers
+ * @returns the id as a String
+ */
+function CreateLobbyID() {
+    let id;
+    do {
+        id = "";
+        for(let i=0; i<6; i++){
+            id += Math.floor(Math.random()*10); 
+        }
+    } while (Rooms.get(`/${id}`));  
+    return id; 
+}
+
+/**
+ * Function for creating the room object that contains the settings and a map for all players
+ * @param {*} socket instance of the users socket
+ * @param {*} username the name given by the player
+ * @param {*} Settings object containing the settings 
+ * @returns the room object
+ */
+function RoomStateObj(socket, username, Settings){
+    // The lobby state is added to the rooms map as a value to the given room id 
+    let LobbyStateObj = { 
+        "players": new Map(),
+        "settings": Settings
+    };
+    LobbyStateObj.players.set(socket.id, CreatePlayer(username, true, socket.id));
+    return LobbyStateObj; 
+}
+
+/**
+ * Function that changes a setting in the Room State
+ * @param {*} ChangeObj object that contains the setting to be changed
+ * @returns boolean whether the setting was accepted or not
+ */
+//change Settings 
+function ChangeSettings(ChangeObj) {
+    if(!isSettingValid(ChangeObj)) {
+        return false; 
+    }
+    const setting = ChangeObj.key;
+    const Room = Rooms.get(`/${ChangeObj.id}`); 
+    Room.settings[setting] = ChangeObj[setting]; 
+    return true; 
+}
+
+/**
+ * Deletes a lobby if no id's are read
+ * @param {*} id Uses to read players id
+ * @param {*} io Allows for access to the overall socket connection
+ */
+function DeleteLobby(id, io){
+    const pathID = `/${id}`;
+    io.to(pathID).socketsLeave(pathID);
+    Rooms.delete(pathID);
+}
+
+/**
+ * tjeks if there are 2 or more players in the room and tjeks if all players in the room are ready. 
+ * @param {*} roomID is the ID of the Room
+ * @returns true or false depending on weather the the conditions for the game to start is met 
+ */
+ 
+function ShouldStartGame(roomID){
+    const players = Rooms.get(roomID).players; 
+    if (players.size < 2) {
+        return false; 
+    }
+    for (const player of players) {
+        if (player.ready === false) {
+            return false;
+        } 
+    }
+    return true;
+}
+
+//* ============================================= joined lobby ============================================================== *\\
+/**
+ * Make it able for players to ready and unready if a deck has been chosen.
+ * @param {*} socketID is the ID for user
+ * @param {*} id is the id for the room
+ * @returns an object 
+ */
+
+function PlayerReady(socketID, id){
+    const pathID = `/${id}`;
+    const Room = Rooms.get(pathID);
+    const Player= Room.players.get(socketID);
+
+    Player.ready = Player.deck === null || Player.ready ? false : true;
+    return {
+        ready: Player.ready,
+        id: socketID
+    }; 
+}
+
+/**
+ * Allows players to join lobby with ther username and id
+ * @param {*} playerArr Includes all players in the lobby
+ * @param {*} PlayerObj Holds the players username
+ * @param {*} socket Holds the players socket id.
+ */
+function JoinLobby(PlayerObj, roomID, socket){
+    socket.join(roomID);
+    const Players = Rooms.get(roomID).players; 
+    const Player = CreatePlayer(PlayerObj.name, false, socket.id); 
+    Players.set(socket.id, Player);
+    
+    const playersArr = MapToArrayObj(Players);
+    return playersArr; 
+}
+
+/**
+ * Allows for users to leave a lobby
+ * @param {*} PlayerObj Gets the players name
+ * @param {*} socket Gets the socket id for the user
+ * @param {*} Room puts the players id into a map.
+ * @param {*} playersleftArr creates a new array with the updated map.
+ */
+function LeaveLobby(PlayerObj, socket){
+    const pathID = `/${PlayerObj.id}`; 
+    const Room = Rooms.get(pathID);
+    
+    //Delete the player from the map
+    Room.players.delete(socket.id);
+    socket.leave(pathID);
+    const playersleftArr = MapToArrayObj(Room.players);
+    return playersleftArr;
+}
+
+//* ====================================================== All users ========================================================= *\\
+/**
+ * Changes the deck for the given user in the room object
+ * @param {*} deckObj contains the deck and id for the room
+ * @param {*} playerID the socket id to recognize the user
+ * @returns a boolean for whether the user is a host or not
+ */
+function ChangeDeckState(deckObj, playerID) {
+    const Room = Rooms.get(`/${deckObj.id}`);
+    if (deckObj.deck > Room.settings.deckSize) {
+        return false;
+    }
+    const player = Room.players.get(playerID); 
+    player.deck = deckObj.deck;
+    if(player.host && !player.ready) {
+        player.ready = true; 
+    }
+    return true; 
+}
+/**
+ * Function for creating a player object for the player map
+ * @param {*} username the username of the user
+ * @param {*} flag to set whether the user is host or not 
+ * @returns the object
+ */
+function CreatePlayer(username, flag) {
+    return {
+        "name": username, 
+        "deck": null,
+        "ready": false,
+        "host": flag
+    };
+} 
+/**
+ * Takes a map and pushes it to an array object
+ * @param {*} map contains an object as the value
+ * @returns the created array
+ */
+function MapToArrayObj(map) {
+    let array = [];
+    for(const [key, value] of map.entries()) { 
+        array.push({ 
+            name: value.name,
+            ready: value.ready,
+            host: value.host,
+            playerid: key
+        }); 
+    }
+    return array;
+}
+
+/**
+ * Function that checks whether the inputted setting is accepted
+ * @param {*} SettingObj the object containing the new setting
+ * @returns boolean is true if the setting is within the range and false if not
+ */
+function isSettingValid(SettingObj) {
+    const setting = SettingObj.key;
+    switch(setting) {
+        case "deckSize": 
+            return SettingObj[setting] >= 15 && SettingObj[setting] <= 50;
+        case "handSize":
+            return SettingObj[setting] >= 5 && SettingObj[setting] <= 7;
+        case "life":
+            return SettingObj[setting] >= 1 && SettingObj[setting] <= 5;
+        case "lobbySize":
+            return SettingObj[setting] >= 2 && SettingObj[setting] <= 30;
+        default: 
+            return false; 
+    }
+}
+
+export { Rooms }; 
--- index.js
+++ index.js
@@ -2,31 +2,142 @@
 import express from "express"
 import http from "http"
 import { Server } from "socket.io"
+import { CreateLobby, ChangeSettings, JoinLobby, LeaveLobby, DeleteLobby, ChangeDeckState, ShouldStartGame, PlayerReady } from "./Lobby.js" // TODO: Make this work
+//import { domainToASCII } from "url"
 const app = express()
 const server = http.createServer(app)
 
 // Socket server that makes use of the above http app server:
 const io = new Server(server, {
-  cors: {
-    origin: "*"
-  }
-})
+	cors: {
+		origin: "*"
+	}
+});
+
+// This map contains all rooms and every room's states
+const Rooms = new Map();
 
 // Basic app routing
 // app.get('/', (req, res) => {
 //  res.sendFile(__dirname + '/index.html');
-// });
+// }); 
 
-// Handle socket connections
-io.on("connection", (socket) => {
-  console.log("a user connected")
+// Handle socket connection
+io.on("connection", socket => {
+	socket.on("disconnect", () => { 
+		console.log(`a user with the id: ${socket.id} has disconnected`);
+	});
 
-  socket.on("buttonClick", (count) => {
-    console.log(socket.id, "has clicked the button", count, "times")
-  })
-})
+	//* ================================================= Lobby Handler ======================================================== *\\
+	socket.on("createLobby", (username) => {
+		console.log("Lobby was created");
+		const CreateLobbyObj = CreateLobby(socket, username);
+		socket.emit("lobby", CreateLobbyObj);
+	});
+	socket.on("changeSettings", (UpdatedSettings) => {
+		const isPossible = ChangeSettings(UpdatedSettings);
+		if(isPossible) {
+			socket.to(`/${UpdatedSettings.id}`).emit("changeSetting", UpdatedSettings);
+		} else {
+			socket.emit("cantChangeSettings");
+		}
+	});
+	socket.on("joinLobby", (Joined) => {
+		const roomID = `/${Joined.id}`;
+		const Room = Rooms.get(roomID);
+		if(Room && Room.players.size < Room.settings.lobbySize) { 
+			const players = JoinLobby(Joined, roomID, socket);
+			socket.to(roomID).emit("playerJoined", JSON.parse(players));
+			
+			//Adds the current settings to the Object for the joining player
+			const JoinedreturnData = { players: players , ...Room.settings };
+			socket.emit("lobby", JoinedreturnData);
+			
+			console.log(Joined.name, "has joined the lobby with id:", roomID);
+		} else if (Room) {
+			socket.emit("RoomFull");
+		} else {
+			socket.emit("roomNotExist");
+		}
+	});
+	socket.on("leaveLobby", (PlayerLeft) => {
+		const roomID = `/${PlayerLeft.id}`; 
+		const playersLeftArr = LeaveLobby(PlayerLeft, socket);
+		socket.to(roomID).emit("playerLeft", {players: playersLeftArr});
+	});
+	socket.on("deleteLobby", (id) => {
+		const roomID = `/${id}`;
+		socket.to(roomID).emit("lobbyDeleted");
+		DeleteLobby(id, socket);
+	});
+	socket.on("chooseDeck", (Deck) => {
+		const player = Rooms.get(`/${Deck.id}`).players.get(socket.id);
+		const isPossible = ChangeDeckState(Deck, socket.id);
+		if(isPossible && player.host) {
+			socket.to(`/${Deck.id}`).emit("hostReadyUp", socket.id);
+		} else if (isPossible) {
+			socket.emit("deckAccepted");
+		} else {
+			socket.emit("deckNotAccepted");
+		}
+	});
 
+	//Listens for player ready and returns the players readyness status.
+	socket.on("playerReady", (id) => {
+		const ReturnPlayerReady = PlayerReady(socket.id, id); 
+		socket.to(`/${id}`).emit("readyUp", ReturnPlayerReady); 
+		socket.emit("readyUp", ReturnPlayerReady);
+	});
+
+	//Listens for a 'startGame' event and either emits a 'startedGame' event to all clients in a room if conditions are met, or sends a 'cantStartGame' event to the initiating client if not.
+	socket.on("startGame", (StartGame) => {
+		const roomID = `/${StartGame.id}`;
+		if(ShouldStartGame(roomID)) {
+			socket.to(roomID).emit("startedGame");
+			socket.emit("startedGame");
+		} else {
+			socket.emit("cantStartGame");
+		}
+	});
+
+	/*socket.on("test", (roomID) => {
+		if(Rooms.get(roomID)) {
+			const Room = Rooms.get(roomID); 
+			console.log(`The room:\n${JSON.stringify(Room)}\n\n`); 
+			console.log("Players in the room");
+			for (const player of Room.players) {
+				console.table(`${JSON.stringify(player)}\n`);
+			}
+		} else {
+			console.log("Room doesnt exist");
+		}
+	});*/
+
+	//* ========================================Battle Page Handler ======================================================= *\\
+
+});
+
+export { Rooms };
 // Start application server
 server.listen(3000, () => {
-  console.log("listening on *:3000")
-})
+	console.log("listening on *:3000");
+});
+
+/*
+	Events - client side: 
+	- lobbyCreated
+	- RoomNotExist
+	- changeSetting
+	- playerJoined
+	- playerLeft
+	- lobbyDeleted
+
+	Events - server side: 
+	- createLobby
+	- changeSettings
+	- joinLobby
+	- leaveLobby
+	- deleteLobby
+	- playerReady
+	- startGame
+*/
