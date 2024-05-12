c3d3553ecabf569b7c8166d6dee41b0cb19806ce
Simon Stjernholm <simon@stjernholm.dev>
2024-05-13 01:39:22 +0200

    optimize switching roles

--- Battle.js
+++ Battle.js
@@ -91,17 +91,15 @@ function nextPlayer(room) {
 
 function switchRoles(roomID, roomData, socket) {
     // Assign new player to select card and new player to answer.
-    // Check if next player is alive, else they should be excluded.
+
+    // Check if current next player is alive, else they should be skipped.
     if(roomData.players.get(roomData.turn.next).lives === 0) {
         // Set next player to a player alive.
         roomData.turn.next = nextPlayer(roomData);
-        // Shift players alive
-        roomData.turn.current = roomData.turn.next;
-        roomData.turn.next = nextPlayer(roomData);
-    } else {
-        roomData.turn.current = roomData.turn.next;
-        roomData.turn.next = nextPlayer(roomData);
     }
+    // Shift players alive
+    roomData.turn.current = roomData.turn.next;
+    roomData.turn.next = nextPlayer(roomData);
 
     socket.to(roomID).emit("switchRoles", { turn: roomData.turn });
     socket.emit("switchRoles", { turn: roomData.turn, hand: roomData.players.get(socket.id).hand });
