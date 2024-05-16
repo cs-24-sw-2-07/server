c31d67dec4f67e7132c16ceeef9ff055e4a05d7c
ChristianSteen-SW23 <cniel@student.aau.dk>
2024-05-16 11:16:41 +0200

    added for deleting card

--- tests/battle.test.js
+++ tests/battle.test.js
@@ -1,28 +1,31 @@
 import { expect, it, describe } from "vitest";
-import { CreateLobby } from "../Lobby";
+import { CreateLobby, ChangeDeckState, JoinLobby, Rooms } from "../Lobby";
 import { PlayerRooms } from "..";
-import { } from "../Battle";
+import { removeCardFromHand } from "../Battle";
 
 
 describe("Battle functions", () => {
 
     it("Removes card from hand", () => {
-        /* // create mock socket object
-         const socketid = "ghu45DxGsxgy5VCls8Zs"
-         const socket = {
+        // create mock socket object
+        const socketid = "ghu45DxGsxgy5VCls8Zs"
+        const socket = {
             id: socketid, // id is 20 random chars.
             join: () => { }
         };
+        //create mock room
         const lobby = CreateLobby(socket, "testuser");
+        const roomID = `/${lobby.id}`
 
         //Give socket 1 a deck
         const deck1 = { name: "test deck", cards: [] };
-        for (let i = 0; i < 15; i++) {
+        for (let i = 0; i < 20; i++) {
             deck1.cards.push({ name: `card${i}`, question: `question${i}`, answer: `answer${i}` });
-        }
-        lobby.get(socketid).players.set("ojIckSD2jqNzOqIrAGzL", { deck: deck1 })*/
+        } 
+        Rooms.get(roomID).players.get(socketid).hand = [1,2,3,4,5];
+        ChangeDeckState(deck1, socket.id, Rooms.get(roomID));
 
-        expect(true).toBe(true);
-        console.log(1)
+        removeCardFromHand(socket.id, 1, roomID)
+        expect(Rooms.get(roomID).players.get(socketid).hand).toStrictEqual([1,3,4,5]);
     })
 })
