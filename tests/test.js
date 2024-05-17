5cbe87ad0020adce4e553e3990134029f0aa0173
Simon Stjernholm <simon@stjernholm.dev>
2024-05-17 11:56:42 +0200

    removed unused file

--- tests/test.js
+++
@@ -1,7 +0,0 @@
-import { expect, it, describe } from "vitest";
-import { removeCardFromHand, drawHand, checkWinner, MapToPlayerLives, nextPlayer, switchRoles } from "../Battle.js";
-import { PlayerRooms } from "..";
-
-describe("Battle functions", () => {
-    
-});
